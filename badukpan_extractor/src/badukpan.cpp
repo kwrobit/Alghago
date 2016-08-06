f094db3f56d30a69b4eb9c271dbf8cfa061cf55f
Suhan Park <psh117@gmail.com>
2016-08-06 18:50:38 +0900

    fix compile error

--- badukpan_extractor/src/badukpan.cpp
+++ badukpan_extractor/src/badukpan.cpp
@@ -6,9 +6,6 @@
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/stitching.hpp>
-#include <opencv2/gpu/gpu.hpp>
-
 
 using namespace std;
 using namespace cv;
