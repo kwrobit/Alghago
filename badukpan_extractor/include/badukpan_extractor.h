e7b4b5af86e5ca691a37e556172be1ec4639c264
Suhan Park <psh117@gmail.com>
2016-08-20 23:59:56 +0900

    badukpan extractor updated

---
+++ badukpan_extractor/include/badukpan_extractor.h
@@ -0,0 +1,43 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <geometry_msgs/PoseArray.h>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <eigen3/Eigen/Dense>
+
+
+using namespace std;
+using namespace cv;
+
+class BadukpanExtractor
+{
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber camera_sub_;
+
+    image_transport::Publisher processed_pub_;
+
+    Mat warpPerspectiveMatrix_;
+
+    bool isFirst_;
+    int count_;
+
+    vector<Eigen::Vector2d> badukalPoints_;
+
+
+public:
+
+    BadukpanExtractor(ros::NodeHandle& nh);
+    virtual ~BadukpanExtractor() {}
+
+
+private:
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+    void fitBadukpan(Mat &srcImg);
+    void scanBadukal(Mat &srcImg, Mat &badukpanImg);
+
+};
+
